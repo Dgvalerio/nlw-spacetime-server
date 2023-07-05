9af1679de51d3607b315fcc1b312f3ad8ae2e0c5
davigvalerio <al.davigvalerio@gmail.com>
2023-07-05 10:33:24 -0300

    feat(auth): create register route

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
-app.register(cors, {
-  origin: true,
-})
+app.register(cors, { origin: true })
 
+app.register(jwt, { secret: 'spacetime' })
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('🚀 HTTP server running on http://localhost:3333')
-  })
+app.listen({ port: 3333 }).then(() => {
+  console.log('🚀 HTTP server running on http://localhost:3333')
+})
