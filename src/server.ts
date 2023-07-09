e1faedc6ade4d122b325082659a05b0e4e887aae
davigvalerio <al.davigvalerio@gmail.com>
2023-07-08 21:14:07 -0300

    feat(uploads): add route to make uploads

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,26 @@ import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import { authRoutes } from './routes/auth'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import fastifyStatic from '@fastify/static'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, { origin: true })
 
 app.register(jwt, { secret: 'spacetime' })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
